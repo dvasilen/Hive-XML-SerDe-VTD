b31eff49d8b22de5f764f81960f1003063816956
Dmitry Vasilenko <dvasilen@gmail.com>
2013-10-01 16:19:50 -0500

    Changed package name

--- src/main/java/com/ximpleware/hive/serde2/xml/vtd/XmlProcessor.java
+++ src/main/java/com/ximpleware/hive/serde2/xml/vtd/XmlProcessor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-package org.apache.hadoop.hive.serde2.xml.vtd;
+package com.ximpleware.hive.serde2.xml.vtd;
 
 import java.util.ArrayList;
 import java.util.HashMap;
